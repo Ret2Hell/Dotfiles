e6df43c954c5121f90cf54ac6d6bdd52224ae02b
Ret2Hell <tayeb1920@gmail.com>
2026-06-07 09:49:55 +0100

    go keymaps/snippets

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,12 +7,33 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-local group = vim.api.nvim_create_augroup("salar-obsidian-markdown", { clear = true })
+local group = vim.api.nvim_create_augroup("ret2hell-obsidian-markdown", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
 	group = group,
 	pattern = "markdown",
 	callback = function(args)
 		require("config.obsidian").setup_markdown_buffer(args.buf)
+	end,
+})
+
+local go_group = vim.api.nvim_create_augroup("ret2hell-go-keymaps", { clear = true })
+
+local function go_term(cmd)
+	return function()
+		Snacks.terminal(cmd, { cwd = LazyVim.root() })
+	end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = go_group,
+	pattern = { "go", "gomod", "gowork", "gotmpl" },
+	callback = function(args)
+		local function map(lhs, rhs, desc)
+			vim.keymap.set("n", lhs, rhs, { buffer = args.buf, desc = desc })
+		end
+
+		map("<leader>cb", go_term({ "go", "build", "./..." }), "Go Build")
+		map("<leader>ct", go_term({ "go", "mod", "tidy" }), "Go Mod Tidy")
 	end,
 })
--- nvim/.config/nvim/lua/config/obsidian.lua
+++ nvim/.config/nvim/lua/config/obsidian.lua
@@ -120,7 +120,7 @@ local function translate_obsidian_datetime_format(format)
 		{ "mm", "%M" },
 		{ "ss", "%S" },
 		{ "A", "%p" },
-		{ "a", "__SALAR_OBSIDIAN_LOWER_AMPM__" },
+		{ "a", "__RET2HELL_OBSIDIAN_LOWER_AMPM__" },
 	}
 
 	local chunks = {}
@@ -167,13 +167,13 @@ local function render_obsidian_datetime(format)
 	local strftime_format = translate_obsidian_datetime_format(format)
 	local rendered = os.date(strftime_format)
 
-	return rendered:gsub("__SALAR_OBSIDIAN_LOWER_AMPM__", function()
+	return rendered:gsub("__RET2HELL_OBSIDIAN_LOWER_AMPM__", function()
 		return os.date("%p"):lower()
 	end)
 end
 
 function M.patch_template_substitutions()
-	if vim.g.salar_obsidian_template_patch_applied then
+	if vim.g.ret2hell_obsidian_template_patch_applied then
 		return
 	end
 
@@ -196,7 +196,7 @@ function M.patch_template_substitutions()
 		return original(text, client, note)
 	end
 
-	vim.g.salar_obsidian_template_patch_applied = true
+	vim.g.ret2hell_obsidian_template_patch_applied = true
 end
 
 function M.setup_markdown_buffer(bufnr)
