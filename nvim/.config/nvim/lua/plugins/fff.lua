32aa442ca9fb6d7cdc54c7a6f8aa30b29863e1a1
Ret2Hell <tayeb1920@gmail.com>
2026-07-08 20:55:35 +0100

    jiratui & herdr completions

--- nvim/.config/nvim/lua/plugins/fff.lua
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -1,38 +1,62 @@
 return {
-	"dmtrKovalenko/fff.nvim",
-	build = function()
-		require("fff.download").download_or_build_binary()
-	end,
-	lazy = false,
-	keys = {
-		{ "<leader><space>", function() require("fff").find_files() end, desc = "Find Files (FFF)" },
-		{ "<leader>ff", function() require("fff").find_files() end, desc = "Find Files (FFF)" },
-		{ "<leader>fF", function() require("fff").find_files_in_dir(vim.fn.getcwd()) end, desc = "Find Files (cwd, FFF)" },
-		{ "<leader>sg", function() require("fff").live_grep() end, desc = "Grep (FFF)" },
-		{
-			"<leader>sw",
-			function()
-				require("fff").live_grep({ query = vim.fn.expand("<cword>") })
-			end,
-			desc = "Word (FFF)",
-		},
-		{
-			"<leader>sG",
-			function()
-				require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
-			end,
-			desc = "Fuzzy Grep (FFF)",
-		},
-	},
-	opts = {
-		lazy_sync = true,
-		prompt_vim_mode = true,
-		layout = {
-			prompt_position = "top",
-			preview_position = "right",
-		},
-		preview = {
-			line_numbers = true,
-		},
-	},
+  "dmtrKovalenko/fff.nvim",
+  build = function()
+    require("fff.download").download_or_build_binary()
+  end,
+  lazy = false,
+  keys = {
+    {
+      "<leader><space>",
+      function()
+        require("fff").find_files()
+      end,
+      desc = "Find Files (FFF)",
+    },
+    {
+      "<leader>ff",
+      function()
+        require("fff").find_files()
+      end,
+      desc = "Find Files (FFF)",
+    },
+    {
+      "<leader>fF",
+      function()
+        require("fff").find_files_in_dir(vim.fn.getcwd())
+      end,
+      desc = "Find Files (cwd, FFF)",
+    },
+    {
+      "<leader>sg",
+      function()
+        require("fff").live_grep()
+      end,
+      desc = "Grep (FFF)",
+    },
+    {
+      "<leader>sw",
+      function()
+        require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+      end,
+      desc = "Word (FFF)",
+    },
+    {
+      "<leader>/",
+      function()
+        require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+      end,
+      desc = "Fuzzy Grep (FFF)",
+    },
+  },
+  opts = {
+    lazy_sync = true,
+    prompt_vim_mode = true,
+    layout = {
+      prompt_position = "top",
+      preview_position = "right",
+    },
+    preview = {
+      line_numbers = true,
+    },
+  },
 }
--- omarchy/.config/omarchy/current/theme/neovim.lua
+++ omarchy/.config/omarchy/current/theme/neovim.lua
@@ -1,12 +1,13 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "tokyonight-night",
-		},
-	},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-nvim",
+    },
+  },
 }
